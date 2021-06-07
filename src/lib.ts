45608d98a20e8ce3f64ae1809bd2faff7b37ac15
Henry Owens <henry.tom.owens@gmail.com>
2021-06-07 22:17:43 +0200

    Adding models exporting

--- src/lib.ts
+++ src/lib.ts
@@ -1,8 +1,11 @@
 import { App } from "@vue/runtime-core";
 import components from "./components";
+import { FormItem } from "./models";
 
 export default {
   install: (app: App): void => {
     app.use(components);
   },
 };
+
+export { FormItem };
