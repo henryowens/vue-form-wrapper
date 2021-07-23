cdff8de71f989c7f485bac8bafd6b4deb9698cd7
Henry Owens <henry.tom.owens@gmail.com>
2021-07-23 15:30:52 +0200

    Fix to typing

--- types/index.d.ts
+++ types/index.d.ts
@@ -1,4 +1,4 @@
-import { DefineComponent, Plugin } from "vue";
+import { Plugin } from "vue";
 import { FormButtonPayload, FormField } from "../src/lib";
 
 declare const vueFormWrapper: Exclude<Plugin["install"], undefined>;
@@ -6,12 +6,3 @@ declare const vueFormWrapper: Exclude<Plugin["install"], undefined>;
 export default vueFormWrapper;
 
 export { FormButtonPayload, FormField };
-
-declare module "*.vue" {
-  const component: DefineComponent<
-    Record<string, unknown>,
-    Record<string, unknown>,
-    any
-  >;
-  export default component;
-}
